b7bc6a121f5608afce3d18a7391b2146de02d856
17rd135 <tbkrk.1112@softbank.ne.jp>
2019-06-24 14:55:18 +0900

    Initial Commit

---
+++ photo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  photo
+//
+//  Created by 津吹陸 on 2019/06/24.
+//  Copyright © 2019 津吹陸. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
